4654cb6db2c5b4e6ea7f1eec3beb22c5dfd39c32
Marcellino Dressel <139843362+marcellinodressel17@users.noreply.github.com>
2024-05-25 02:16:49 +0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -110,15 +110,11 @@
 const logoutButton = document.querySelector('.logout-button');
 
 logoutButton.addEventListener('mouseover', () => {
-  // Store the original button text (username)
   const originalText = logoutButton.textContent;
-
-  // Change button text to 'Logout' on hover
   logoutButton.textContent = 'Logout';
 
-  // Add an event listener for mouseout to revert text
   logoutButton.addEventListener('mouseout', () => {
-    // Revert button text to original text (username)
+  
     logoutButton.textContent = originalText;
   });
 });
